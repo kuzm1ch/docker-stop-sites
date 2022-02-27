8dadfd1fa8d0632db3b38c6fd8799cb710664549
Mykhailo Kyzmich <kuzmichm.its@gmail.com>
2022-02-27 15:42:29 +0200

    fix: OOM issue when chrome page is always stay open | reopen page each 10 minutes

--- script.sh
+++ script.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-  
+
 # turn on bash's job control
 set -m
-  
+
 # Start the primary process and put it in the background
+# rendertron - headless Chrome rendering solution
 npm run start &
 
-sleep 10
-# Start the helper process 
-for i in {1...1000}; do curl http://localhost:3000/render/https://stop-russian-desinformation.near.page/; done
-
-# the my_helper_process might need to know how to wait on the
+# the curl  might need to know how to wait on the
 # primary process to start before it does its work and returns
+sleep 10
+
+# Start open stop-russions-desinform site
+for i in {1..1000}; do curl http://localhost:3000/render/https://stop-russian-desinformation.near.page/ && sleep 15; done &
 
 # now we bring the primary process back into the foreground
 # and leave it there
 fg %1
+
+# Wait for any process to exit
+wait -n
+
+# Exit with status of process that exited first
+exit $?
